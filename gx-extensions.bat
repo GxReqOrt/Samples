ecd50fd32644c038dc3ca6ca2620aa5466a29e67
Alvaro Nicoli <xmr.nkr@gmail.com>
2022-01-16 14:01:45 -0300

    docs: readme and license

--- gx-extensions.bat
+++ gx-extensions.bat
@@ -5,7 +5,6 @@ rem run on visual studio developer console as admin
 set GX_PROGRAM_DIR=C:\Program Files (x86)\GeneXus\GeneXus17
 set GX_SDK_DIR=C:\Program Files (x86)\GeneXus\GeneXus17PlatformSDK
 
-set SLN_PATH
 for /f "delims=" %%a in ('dir /b/s *.sln') do set SLN_PATH=%%a 
 
 devenv %SLN_PATH%
